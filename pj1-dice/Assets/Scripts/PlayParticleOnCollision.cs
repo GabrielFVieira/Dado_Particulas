e89b9b78d7703eaa010caf7a837adef95500b218
JackFury360 <Gabriel.Vieira@cejoseleitelopes.com.br>
2017-11-22 11:49:02 -0200

    First Commit

---
+++ pj1-dice/Assets/Scripts/PlayParticleOnCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayParticleOnCollision : MonoBehaviour
+{
+	public ParticleSystem[] particles;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+
+    private void OnCollisionEnter( Collision collision )
+	{
+		foreach( var particle in particles )
+		{
+			particle.Play();
+		}
+	}
+}
